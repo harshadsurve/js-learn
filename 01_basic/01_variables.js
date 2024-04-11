8fda2c1e9d071f6b0ad5300082e493d0b9efa3d8
harshadsurve <144141453+harshadsurve@users.noreply.github.com>
2024-04-11 06:39:24 +0000

    variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 12345
+/*
+ The const declaration declares block-scoped local variables.
+ The value of a constant can't be changed through reassignment using the assignment operator
+ */
+let accountEmail="abc11@gmail.com"
+/*The let declaration declares re-assignable, block-scoped local variables, 
+optionally initializing each to a value.
+*/
+var accountPassowrd="12345"
+/* prefer not to use var
+ because of issue in block scope and functional scope
+*/
+accountCity="karad"
+
+ // accountId= 2 // not allowed
+accountEmail="adc12@gamil.com"
+accountPassowrd="12345"
+accountCity="patan"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassowrd,accountCity]);
+//The console.table() static method displays tabular data as a table.
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";// treat all js code as newer version
+
+//alert("hello")// we are using nodejs, not browser
+
+console.log(3+3)
+// code readablitty should be high
+console.log("harshad")
+
+let name="harshad"
+let age=18
+let isLoggedIn= false
+let state;
+// number => 2 to power 53
+// bigint =>The BigInt type is a numeric primitive in JavaScript that can represent integers with arbitrary magnitude
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol  => unique immutable data type
+// object=> {} or []
+console.log(typeof undefined);// undefined type in js
+console.log(typeof null);// object
+
