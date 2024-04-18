43026f8407cac02e21c6c7d0ff334d0ae538024c
harshadsurve <144141453+harshadsurve@users.noreply.github.com>
2024-04-18 10:59:54 +0000

    String

--- 01_basics/06_string.js
+++ 01_basics/06_string.js
@@ -32,3 +32,33 @@ console.log(url.replace('%20','-'));
 console.log(url.includes('harsh'));
 
 console.log(gameName.split('-'));
+
+/*
+Basic String Methods
+Javascript strings are primitive and immutable: 
+All string methods produces a new string without altering the original string.
+
+String length
+String charAt()
+String charCodeAt()
+String at()
+String [ ]
+String slice()
+String substring()
+String substr()
+See Also:
+String Search Methods
+String Templates
+String toUpperCase()
+String toLowerCase()
+String concat()
+String trim()
+String trimStart()
+String trimEnd()
+String padStart()
+String padEnd()
+String repeat()
+String replace()
+String replaceAll()
+String split()
+*/
