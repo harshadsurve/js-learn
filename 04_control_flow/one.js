25d067d1a6e62ba3501ff83bed4bf2298ba8084f
harshadsurve <144141453+harshadsurve@users.noreply.github.com>
2024-05-27 15:05:46 +0000

    if

---
+++ 04_control_flow/one.js
@@ -0,0 +1,65 @@
+/*
+JavaScript control statement is used to control the execution of a program based on a specific condition. 
+If the condition meets then a particular block of action will be executed otherwise
+ it will execute another block of action that satisfies that particular condition.
+*/
+
+//comper operatoe  < , > , <= , >= , == , != , === , !==
+
+// if 
+
+const isUserLoggedIn = true
+if (isUserLoggedIn) {
+    
+}
+if (2 === "2"){
+    console.log("executed");
+}
+
+
+if( 2 != 3){// true
+    console.log("executed");
+
+}
+
+const temperature = 41
+// if (temperature < 50) {
+//     console.log("less than 50");
+// }else{
+// console.log("temperature is greater than 50");
+// }
+// console.log("executed");
+
+const score = 200 
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`User power ${power}`);
+// }
+
+// console.log(`User power ${power}`); -> ReferenceError: power is not defined
+
+
+// short hand 
+const balance = 1000
+// if(balance > 500 ) console.log("test"), console.log("test2"); do not use this 
+
+// nesting 
+// if(balance < 500){
+//    console.log("less than 500");
+// } else if(balance < 750){
+//     console.log("less than 750");
+
+// }else if(balance < 900){
+//      console.log("less than 900");
+
+// }else {
+//     console.log("less than 1200");
+
+// }
+
+const isUserLoggedIn1 = true
+const debitcard = true
+
+if (is) {
+    
+}
