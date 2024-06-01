c8881c945b37222687b8eeadae77b9cf51e8127b
harshadsurve <144141453+harshadsurve@users.noreply.github.com>
2024-06-01 13:46:50 +0530

    Update 06_string.js

--- 01_basics/06_string.js
+++ 01_basics/06_string.js
@@ -12,10 +12,12 @@ console.log(gameName.__proto__);
 
 console.log(gameName.length);
 console.log(gameName.toUpperCase());
-console.log(gameName.charAt(6));
-console.log(gameName.indexOf('a'));
+console.log(gameName.charAt(6));//The charAt() method of String values returns a new string consisting of the single UTF-16 code unit at the given index.
+console.log(gameName.indexOf('a'));//The JavaScript String indexOf() method finds the index of the first occurrence of the argument string in the given string. The value returned is 0-based.
 
-const newString = gameName.substring(0,4)
+const newString = gameName.substring(0,4)//The substring() method of String values returns the part of this string from the start index up to and excluding the end index, or to the end of the string if no end index is supplied.
+
+
 console.log(newString); 
 
 const anotherString = gameName.slice(-7,4)
@@ -27,10 +29,23 @@ console.log(newStringOne.trim());
 
 const url = "https://harsh.com/harsh%20surve"
 
+/*
+The replace() method of String values returns a new string with one, some, or all matches of a pattern replaced by a replacement.
+The pattern can be a string or a RegExp, and the replacement can be a string or a function called for each match. If pattern is a string,
+only the first occurrence will be replaced. The original string is left unchanged.
+*/
 console.log(url.replace('%20','-'));
 
+/*
+The includes() method of String values performs a case-sensitive search to determine whether a given string may be found within 
+this string, returning true or false as appropriate.
+*/
 console.log(url.includes('harsh'));
 
+/*
+The split() method of String values takes a pattern and divides this string into an ordered list of substrings by searching for 
+the pattern, puts these substrings into an array, and returns the array.
+*/
 console.log(gameName.split('-'));
 
 /*
