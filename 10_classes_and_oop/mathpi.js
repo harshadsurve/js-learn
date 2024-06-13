afa0bd040860a15becf9822e468c15e0ecf8fb29
harshadsurve <144141453+harshadsurve@users.noreply.github.com>
2024-06-13 10:28:02 +0000

    bind and math.pi mestri

---
+++ 10_classes_and_oop/mathpi.js
@@ -0,0 +1,32 @@
+const Descriptor= Object.getOwnPropertyDescriptor(Math,'PI')
+// console.log(Descriptor);
+
+// console.log(Math.PI);
+// Math.PI= 5
+// console.log(Math.PI);
+
+const chai={
+    name:'ginger',
+    price:250,
+    isAvaliable:true,
+
+
+    orderchai:function(){
+        console.log(`chai nahi bani`);
+    }
+}
+console.log(Object.getOwnPropertyDescriptor(chai,'name'));
+
+Object.defineProperty(chai,'name',{ 
+    // writable: false,
+    enumerable: false,
+
+    })
+    console.log(Object.getOwnPropertyDescriptor(chai,'name'));
+for (let [key,value] of Object.entries(chai)) {
+    if (typeof value !== 'function') {
+        console.log(`${key}:${value}`);
+
+    }
+};
+    
